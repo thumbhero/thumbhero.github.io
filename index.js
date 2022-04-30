0e0e217c9e48522f06c89408940e2d69bc68aab6
thumbhero <hsc1119@gmail.com>
2022-04-30 12:29:49 +0900

    fix bug for tokyo lat

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Initialize and add the map
 function initMap() {
     // The location of Uluru
-    const tokyo = { lat: 135.685174508463774, lng: 139.75240159042139 };
+    const tokyo = { lat: 35.685174508463774, lng: 139.75240159042139 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 3,
+      zoom: 13,
       center: tokyo,
     });
     // The marker, positioned at Uluru
