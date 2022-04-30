e16034c9b4de9034216e86525787bc6d8bc22796
thumbhero <hsc1119@gmail.com>
2022-04-30 12:51:52 +0900

    fix comment

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Initialize and add the map
 function initMap() {
-    // The location of Uluru
+    // The location of Tokyo
     const tokyo = { lat: 35.685174508463774, lng: 139.75240159042139 };
-    // The map, centered at Uluru
+    // The map, centered at Tokyo
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 13,
       center: tokyo,
     });
-    // The marker, positioned at Uluru
+    // The marker, positioned at Tokyo
     const marker = new google.maps.Marker({
       position: tokyo,
       map: map,
